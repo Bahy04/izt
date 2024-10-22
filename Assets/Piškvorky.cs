cb06b603759953887960c78e78d54a9bd26b9946
Bahy04 <bahenskyjosef87@gmail.com>
2024-10-22 13:22:46 +0200

    piskvorky 2

---
+++ Assets/Piškvorky.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Boolean checker;
+    int plusone;
+
+    public Text text = null;
+
+    public void score()
+    {
+
+    }
+
+}
